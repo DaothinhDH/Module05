3091b09abdd9f400059171bdc98422c8bf4c6432
DaothinhDH <138435806+DaothinhDH@users.noreply.github.com>
2024-01-04 11:27:21 +0700

    Add files via upload
    ss8

---
+++ securitymd5/src/main/java/com/ra/controller/Demo.java
@@ -0,0 +1,14 @@
+package com.ra.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class Demo {
+    @GetMapping("/403")
+    public  String demo(){
+        return "403";
+    }
+
+}
